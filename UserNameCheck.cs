463b02681a82bca47700cf3d04e9e9c8a6bf8b80
Aishwarya Rahatal <aishwarya.rahatal97@gmail.com>
2021-10-04 14:02:34 +0530

    regex for name

---
+++ UserNameCheck.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class UserNameCheck
+    {
+        public string RegexName= "^[A-Z]{1}[a-z]{3,}$";
+
+        public void CheckName(string name)       
+        {
+           bool check= Regex.IsMatch(name, RegexName);
+
+            if(check==true)
+            {
+                Console.WriteLine(name + " Name is Valid !");
+
+            }
+            else
+            {
+                Console.WriteLine(name +  " Name is Not Valid \n");
+                
+
+            }
+
+        }
+
+       
+
+
+    }
+}
+
