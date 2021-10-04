835adc99782b83d5bf712a6aab5d6d88580ac845
Aishwarya Rahatal <aishwarya.rahatal97@gmail.com>
2021-10-04 14:15:39 +0530

    regex for password

---
+++ UserPasswordCheck.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class UserPasswordCheck
+    {
+        public string RegexPaasword = "((?=.*\\d)(?=.*[A-Z])(?=.*\\W).{8,})";
+
+        public void CheckPassword(string password)
+        {
+            bool check = Regex.IsMatch(password, RegexPaasword);
+
+            if (check == true)
+            {
+                Console.WriteLine(password + " Password is Valid !");
+
+            }
+            else
+            {
+                Console.WriteLine(password + " Password is Not Valid \n");
+               
+
+            }
+
+        }
+    }
+}
+
